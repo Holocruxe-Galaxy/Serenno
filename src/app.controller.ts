cbc9a270b3578f22f3250634c6b2955e6a74f7f0
EmiCon97 <derleuchtturm97@gmail.com>
2023-07-28 23:04:49 -0300

    notification url

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { AppService } from './app.service';
 
@@ -12,5 +12,27 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/notification')
+  notification() {
+    return;
+  }
+
+  @Post('/order')
+  async createOrder(@Body() data) {
+    const createdOrder = this.appService.createOrder(data);
+
+    this.httpService
+      .get('https://api.mercadolibre.com/items/MLA1380266233', {
+        headers: {
+          Authorization: `Bearer APP_USR-8119640945694263-070700-dbeea63075ad777f5ff43ae55c233f6c-1156166910`,
+        },
+      })
+      .subscribe((value) => {
+        console.log(value);
+      });
+
+    return createdOrder;
   }
 }
