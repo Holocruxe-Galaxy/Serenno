87cca0edfef1f0392c25a1a5624af3592defbc64
EmiCon97 <derleuchtturm97@gmail.com>
2023-07-30 18:31:24 -0300

    port

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, ValidationPipeOptions } from '@nestjs/common';
 import { AppModule } from './app.module';
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 export const validationOptions: ValidationPipeOptions = {
   whitelist: true,
